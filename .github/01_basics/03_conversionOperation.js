a6fd90da1c680dc821968ef62e12099bff401992
Devanssharma116 <141948763+Devanssharma116@users.noreply.github.com>
2024-05-13 06:37:28 +0000

    conversion of datatypes

---
+++ .github/01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+// let score="33"
+// let score1="33abc"
+// console.log(typeof score)
+// let valuenumber=Number(score)
+// console.log(typeof valuenumber)
+// let valuenumber1=Number(score1)
+// console.log(typeof valuenumber1)
+// console.log(valuenumber1)
+
+// let score2=null
+// let valuenumber2=Number(score2)
+// console.log(typeof score2)
+// console.log(score2)
+// console.log(typeof valuenumber2)
+// console.log(valuenumber2)
+
+// let score2=undefined
+// let valuenumber2=Number(score2)
+// console.log(typeof score2)
+// console.log(score2)
+// console.log(typeof valuenumber2)
+// console.log(valuenumber2)
+
+// let score2=true
+// let valuenumber2=Number(score2)
+// console.log(typeof score2)
+// console.log(score2)
+// console.log(typeof valuenumber2)
+// console.log(valuenumber2)
+
+// let score2="sriji"
+// let valuenumber2=Number(score2)
+// console.log(typeof score2)
+// console.log(score2)
+// console.log(typeof valuenumber2)
+// console.log(valuenumber2)
+
+
+// let isloggin=1
+// let isloggin=""
+let isloggin="abcd123"
+let booleanIsloggin=Boolean(isloggin)
+console.log(booleanIsloggin)
+/* 
+1==> true; 0==>false 
+"" ==>false
+"abcdef"==>true
+
+*/
+let somnumber=33
+console.log(String(somnumber))
+console.log(typeof String(somnumber))
